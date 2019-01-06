591f1d63e7703d3a03c89677a7082d389ff4ae7f
Weber <xswzaq44321@gmail.com>
2019-01-06 11:26:02 +0800

    Modify flandre radiate behavior in lua

--- Assets/StreamingAssets/scripts/flandre/control.lua
+++ Assets/StreamingAssets/scripts/flandre/control.lua
@@ -1,14 +1,20 @@
 local count = 0
 local a = 0
 
+local radiate
+
 function boss.upDate()
-	count = count + timer.deltaTime;
-	if(count > 2)then
-		if(count > 3)then
+	radiate()
+end
+
+function radiate()
+	count = count + timer.deltaTime
+	if(count > 3)then
+		if(count > 3.5)then
 			count = 0
 			a = 0
 		end
-		a = a + 1
+		a = a + 0.5
 		boss.radiate("pink_load", "pink_circle_small", 1, 4.4, a * 11, 2 - a * 0.01)
 		boss.radiate("pink_load", "pink_circle_small", 1, 5.0, a * 11, 2 - a * 0.01)
 		boss.radiate("pink_load", "pink_circle_small", 1, 5.6, a * 11, 2 - a * 0.01)
@@ -17,5 +23,16 @@ function boss.upDate()
 		boss.radiate("pink_load", "pink_circle_small", 1, 5.0, -a * 11, 2 - a * 0.01)
 		boss.radiate("pink_load", "pink_circle_small", 1, 5.6, -a * 11, 2 - a * 0.01)
 		boss.radiate("pink_load", "pink_circle_small", 1, 6.2, -a * 11, 2 - a * 0.01)
+
+		if(a < 50)then
+			boss.radiate("cyan_load", "cyan_seed", 1, 1.4, a * 11 + 180, 1.5 - a * 0.02);
+			boss.radiate("cyan_load", "cyan_seed", 1, 2.0, a * 11 + 180, 1.5 - a * 0.02);
+			boss.radiate("cyan_load", "cyan_seed", 1, 2.6, a * 11 + 180, 1.5 - a * 0.02);
+			boss.radiate("cyan_load", "cyan_seed", 1, 3.2, a * 11 + 180, 1.5 - a * 0.02);
+			boss.radiate("cyan_load", "cyan_seed", 1, 1.4, -a * 11 + 180, 1.5 - a * 0.02);
+			boss.radiate("cyan_load", "cyan_seed", 1, 2.0, -a * 11 + 180, 1.5 - a * 0.02);
+			boss.radiate("cyan_load", "cyan_seed", 1, 2.6, -a * 11 + 180, 1.5 - a * 0.02);
+			boss.radiate("cyan_load", "cyan_seed", 1, 3.2, -a * 11 + 180, 1.5 - a * 0.02);
+		end
 	end
 end
