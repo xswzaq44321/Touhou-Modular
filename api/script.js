96c28dfbbe878779315bc6be913031bce3668214
Weber <xswzaq44321@gmail.com>
2019-01-08 16:30:25 +0800

    Add bullet enum

--- api/script.js
+++ api/script.js
@@ -2,5 +2,8 @@ $(document).ready(function () {
 	$('#keycode').click(function () {
 		$(this).parents().next('.hide').toggle();
 	});
+	$('#keycode2').click(function () {
+		$(this).parents().next('.hide').toggle();
+	});
 	$('.hide').toggle();
 });
